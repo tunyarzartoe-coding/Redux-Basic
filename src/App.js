80a7cc5d18b7106b8f073555dd2a01a893ec5f08
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-24 23:13:28 +0630

    [Complete] reducx-counter-app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Counter from "./features/counter/Counter";
 function App() {
   return (
     <main>
-      <h2>Redux Counter</h2>
       <Counter/>
     </main>
     
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
-import { increment,decrement } from "./counterSlice";
-import { reset, incrementByAmount } from "./counterSlice";
+import { increment, decrement, reset, incrementByAmount } from "./counterSlice";
 import { useState } from "react";
+
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
   const [incrementAmount, setIncrementAmount] = useState(0);
@@ -12,43 +12,61 @@ const Counter = () => {
     setIncrementAmount(0);
     dispatch(reset());
   };
+
   return (
-    <section>
-      <p>{count}</p>
-      <div>
-        <button
-          onClick={() => {
-            dispatch(increment());
-          }}
-        >
-          Add
-        </button>
-        <button
-          onClick={() => {
-            dispatch(decrement());
-          }}
-        >
-          Sub
-        </button>
-      </div>
-
-      <input
-        type="number"
-        value={incrementAmount}
-        onChange={(e) => setIncrementAmount(e.target.value)}
-      /> 
-
-      <div>
-        <button onClick={resetHandler}>Reset</button>
-        <button
-          onClick={() => {
-            dispatch(incrementByAmount(addValue));
-          }}
-        >
-          AddbyAmount
-        </button>
-      </div>
-    </section>
+    <div className="container" style={{}}>
+      <section className="card">
+        <div className="card-header">
+          <h4>Redux Counter App</h4>
+        </div>
+        <div className="card-body">
+          <button
+            type="buttton"
+            className="btn btn-outline-info count-display w-50 p-0"
+          >
+            {count}
+          </button>
+          <div className="button-group">
+            <button
+              type="button"
+              className="btn btn-success"
+              onClick={() => dispatch(increment())}
+            >
+              Add
+            </button>
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={() => dispatch(decrement())}
+            >
+              Sub
+            </button>
+          </div>
+          <input
+            type="number"
+            value={incrementAmount}
+            onChange={(e) => setIncrementAmount(e.target.value)}
+            className="input-field w-100 text-center"
+          />
+          <div className="button-group d-flex">
+            <button
+              type="button"
+              className="btn btn-danger"
+              onClick={resetHandler}
+            >
+              Reset
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => dispatch(incrementByAmount(addValue))}
+            >
+              Add by Amount
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
